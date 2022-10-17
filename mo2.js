8fa2ea15cc76850c91cf265f5d9fd8d47587f238
abdalrhmanndev <91363994+abdalrhmanndev@users.noreply.github.com>
2022-10-16 22:04:22 -0700

    Add files via upload

---
+++ mo2.js
@@ -0,0 +1,138 @@
+
+
+// const tb =[
+// {name:"تحضيرية" , num:2330},
+// {name:" تحضيرية موازي"  ,num:2310},
+// {name:"الهندسة الطبية" ,num:2260},
+// {name:"كلية الهندسة الحيوية" , num:2000},
+// {name:"المعهد تعويضات سنية" , num:2080},
+// {name:"كلية العلوم الصحية" , num:2000},
+// {name:"كلية الكيمياء" , num:1700},
+// {name:"كلية الجلوجيا" , num:1700},
+// {name:"كلية الطب البيطري" , num:1700},
+// {name:"كلية الزراعة" , num:1700},
+// {name:"كلية العلوم " , num:1590},
+// {name:"كلية علم الاجتماع" , num:1450},
+// {name:"المعهد الزراعي" , num:1160},
+// {name:"المعهد البيطري" , num:1160},
+// {name:"المعهد الصحي" , num:1160},
+// {name:"مدرسة التمريض" , num:1160},  
+
+// ]
+ 
+
+// const teach =[
+// {name:"الهندسة الغذائية" , num:2000},
+// {name:"كلية العلوم الصحية" , num:2000 },
+// {name:"تعويضات سنية" , num:2080},
+// {name:"الاعلام" , num:1970},
+// {name:"كلية الحقوق" , num:1870},
+// {name:"كلية العلوم السياسية" , num:1700},
+// {name:"كلية السياحة" , num:1590},
+// {name:"كلية التربية" , num:1450},
+// {name:"كلبة الأداب" , num:1450},
+// {name:"كلية الشريعة" , num:1440},
+// {name:"المعهد المتوسط للشرعية" , num:1250},
+// {name:"كلية التربية الرياضية" , num:1200},
+// {name:"المعهد الفندقي" , num:1160},
+// {name:"معهد الحاسوب" , num:1160},
+// {name:"المعهد القانوني" , num:1160},
+
+// ]
+
+// const cs =[
+// {name:"الهندسة المعلوماتية" , num:2260 },
+// {name:"هندسة الحاسوب" , num: 2190},
+// {name:"هندسة الانصالات" , num:2190 },
+// {name:"الهندسة الاكهربائية" , num:2160 },
+// {name:"هندسة المكاترونيكس" , num:2120 },
+// {name:"الكلية التطبيقية" , num:2000 },
+// {name:"كلية التحكم الألي" , num:2000 },
+// {name:"كلية الاقتصاد" , num:1870 },
+// {name:"كلية الاحصاء الرياضي" , num:1700 },
+// {name:"كلية الفيزياء" , num:1590 },
+// {name:"كلية الرياضيات" , num:1590 },
+// {name:"كلية العلوم الادارية" , num:1250 },
+// {name:"معهد الحاسوب" , num:1160 },
+// {name:"المعهد الاحصائي" , num:1160 },
+// {name:"المعهد المالي" , num:1160 },
+
+// ]
+
+
+// const goe=[
+//     {name:"الهندسة الطبية" , num:2260},
+//     {name:"الهندسة المدنية" , num:2190},
+//     {name:"هندسة الاتصالات" , num:2190},
+//     {name:"هندسة الالكترونات" , num:2160},
+//     {name:"هندسة المكاترونيكس" , num:2120 },
+//     {name:"الندسة البتروكيمائية" , num:2080},
+//     {name:"الهندسة الكيميائية" , num:2080},
+//     {name:"هندسة البترول" , num:2080},
+//     {name:"الهندسة المعمارية" , num:2000},
+//     {name:"الهندسة الكهرباثية" , num:2000},
+//     {name:"هندسة الغزل" , num:1970},
+//     {name:"المعهد النفط" , num:1970},
+//     {name:"الهندسة المكانيكية" , num:1870},
+//     {name:"كلية الرياضيات" , num:1870 },
+//     {name:"كلية الكيمياء" , num:1700},
+//     {name:"كلية الاحصاء الرياضي" , num:1700 },
+//     {name:"كلية الفيزياء" , num:1590 },
+//     {name:"كلية الجولوجيا" , num:1450 },
+//     {name:"المعهد الهندسي" , num:1160},
+//     {name:"المعهد البحري" , num:1160},
+//     {name:"المعهد الكهربائي" , num:1160},
+//     {name:"المعهدالصناعي" , num:1160},
+    
+//     ]
+ 
+
+
+// // let td =document.createElement('td');
+
+// // for(i=0; i<tb.length; i++){
+// // let name_txt =document.createTextNode('hi');
+
+     
+      
+// // let tr =document.createElement('tr');
+// // tr.appendChild(td);
+
+// // table.appendChild(tr);
+
+
+
+// let table =document.getElementById('table');
+
+// for(i=0 ; i<tb.length ;i++){
+
+// // 1
+
+//       let tr =document.createElement('tr');
+
+//       let td1 =document.createElement('td');
+//       let hash_txt = document.createTextNode(i+1);
+//       td1.appendChild(hash_txt);
+//       tr.appendChild(td1);
+
+//       // 2
+
+//       let td2 =document.createElement('td');
+//       let name_txt = document.createTextNode(tb[i].name);
+//       td2.appendChild(name_txt);
+//       tr.appendChild(td2);
+
+//       // // 3
+
+//       let td3 =document.createElement('td');
+//       let num_txt = document.createTextNode(tb[i].num);
+//       td3.appendChild(num_txt);
+//       tr.appendChild(td3);
+
+ 
+// table.appendChild(tr);
+
+//       }
+
+alert('LD')
+      
